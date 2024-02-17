0d4008fdab2abefb0ae5a372ab0e040a44be74ad
ys-grouse <malsawma7777@gmail.com>
2024-02-17 08:29:10 +0530

    feat[add]: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //seed super user
+        $su = User::factory()->create([
+            'name' => 'SU',
+            'email' => 'admin@example.email',
+            'password' => bcrypt('admin@example.email')
+        ]);
     }
 }
