084bcd6fb9fe6db3622a77d2a55589d5b1cfdab0
ys-grouse <malsawma7777@gmail.com>
2024-02-18 00:00:13 +0530

    feat[add]: can access panel method

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 use BezhanSalleh\FilamentShield\Traits\HasPanelShield;
 use BezhanSalleh\FilamentShield\Traits\HasWidgetShield;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -47,4 +48,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
