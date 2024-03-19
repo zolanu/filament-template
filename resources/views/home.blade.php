ca6740f762715fa63702a42c9233bcdaf4cd2409
ys-grouse <malsawma7777@gmail.com>
2024-03-20 01:30:10 +0530

    chore[add]: home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Zolanu Filament</title>
+</head>
+
+<body style="text-align: center; color:#dfdfdf; background: #232323">
+    <h1 class="" style="padding-top: 10vh">THIS IS ZOLANU APP</h1>
+    <h3 style="">
+        Zolanu has no content
+    </h3>
+</body>
+
+</html>
